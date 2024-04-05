e73197767ccd32b6783533c7f31c77f1ff242284
tjxt <tjxt@itcast.cn>
2024-04-05 12:44:10 +0800

    add: Q21 Solution2

--- src/main/java/Q21/Main.java
+++ src/main/java/Q21/Main.java
@@ -14,8 +14,9 @@ public class Main {
         node5.next = node6;
 
         Solution solution = new Solution();
-        ListNode res = solution.mergeTwoLists(node1,node4);
-        while(res.next != null) {
+        Solution2 solution2 = new Solution2();
+        ListNode res = solution2.mergeTwoLists(node1,node4);
+        while(res != null) {
             System.out.println(res.val);
             res = res.next;
         }
@@ -61,6 +62,34 @@ class Solution {
             res.next = new ListNode(l2.val);
             res = res.next;
             l2 = l2.next;
+        }
+
+        return temp.next.next;
+    }
+}
+class Solution2 {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode l1 = list1;
+        ListNode l2 = list2;
+        ListNode res = new ListNode(-1);
+        ListNode temp = new ListNode(0,res);
+
+        while(l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                res.next = l1;
+                res = res.next;
+                l1 = l1.next;
+            } else {
+                res.next = l2;
+                res = res.next;
+                l2 = l2.next;
+            }
+        }
+        if (l1 != null) {
+            res.next = l1;
+        }
+        if (l2 != null) {
+            res.next = l2;
         }
 
         return temp.next.next;
